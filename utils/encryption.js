938eb955950edab67f1464f160fc21405c90ab77
komallsingh <singhkomal11711@gmail.com>
2026-05-19 01:56:19 +0530

    encryption

---
+++ utils/encryption.js
@@ -0,0 +1,49 @@
+const crypto = require("crypto");
+
+const algorithm = "aes-256-cbc";
+
+const key = crypto
+    .createHash("sha256")
+    .update(process.env.SECRET_KEY)
+    .digest();
+
+const iv = Buffer.alloc(16, 0);
+
+const encrypt = (text) => {
+
+    const cipher = crypto.createCipheriv(
+        algorithm,
+        key,
+        iv
+    );
+
+    let encrypted = cipher.update(text, "utf8", "hex");
+
+    encrypted += cipher.final("hex");
+
+    return encrypted;
+};
+
+const decrypt = (encryptedText) => {
+
+    const decipher = crypto.createDecipheriv(
+        algorithm,
+        key,
+        iv
+    );
+
+    let decrypted = decipher.update(
+        encryptedText,
+        "hex",
+        "utf8"
+    );
+
+    decrypted += decipher.final("utf8");
+
+    return decrypted;
+};
+
+module.exports = {
+    encrypt,
+    decrypt
+};
